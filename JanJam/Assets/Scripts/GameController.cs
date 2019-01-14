2a9183e0780a16e596cab2a6f9f091f50fb67bc5
Jonathan Carter <jmc@devj.uk>
2019-01-14 11:19:14 +0000

    Unicycle in scene

---
+++ JanJam/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Scenes
+{
+	Menu,
+	Level,
+	End,
+	Win,
+};
+
+
+public class GameController : MonoBehaviour
+{
+
+	public Scenes Scene;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
