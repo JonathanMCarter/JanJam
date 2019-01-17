2acfd9e51ab5406fdacfa1220815db7db16de98c
Jonathan Carter <jmc@devj.uk>
2019-01-17 14:39:21 +0000

    Work pt3

---
+++ JanJam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public bool SceneChanged;                                                                   // Bool for whether the scene has changed or not
+	public GameObject Sound_Prefab;                                                             // Holds the prefab that plays the sound requested
+	public List<string> Sound_Names = new List<string>();                                       // A list to hold the audioclip names
+	public List<AudioClip> Sound_Clips = new List<AudioClip>();                                 // A list to hold the audioclips themselves
+	public Dictionary<string, AudioClip> Sound_Lib = new Dictionary<string, AudioClip>();       // Dictionary that holds the audio names and clips
+
+
+	private void Start()
+	{
+		for (int i = 0; i < Sound_Names.Count; i++)         // For loop that populates the dictionary with all the sound assets in the lists
+		{
+			Sound_Lib.Add(Sound_Names[i], Sound_Clips[i]);
+		}
+	}
+
+
+	public void PlaySound(string request)                   // Fuction to select and play a sound asset from the start and destroy the prefab once it has played
+	{
+		if (Sound_Lib.ContainsKey(request))                                 // If the sound is in the library
+		{
+			GameObject clip = Instantiate(Sound_Prefab);                        // Instantiate a Sound prefab
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];         // Get the prefab and add the requested clip to it
+			clip.GetComponent<AudioSource>().Play();                            // play the audio from the prefab
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);        // Destroy the prefab once the clip has finished playing
+			ChangeVolume(1);                                                    // reset the volume if it was altered
+		}
+	}
+
+
+
+	public void PlaySound(string request, float loudness)                   // Fuction to select and play a sound asset from the start and destroy the prefab once it has played
+	{
+		if (Sound_Lib.ContainsKey(request))                                 // If the sound is in the library
+		{
+			GameObject clip = Instantiate(Sound_Prefab);                        // Instantiate a Sound prefab
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];         // Get the prefab and add the requested clip to it
+			clip.GetComponent<AudioSource>().volume = loudness;
+			clip.GetComponent<AudioSource>().Play();                            // play the audio from the prefab
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);        // Destroy the prefab once the clip has finished playing
+			ChangeVolume(1);                                                    // reset the volume if it was altered
+		}
+	}
+
+
+
+	public void PlayFromTime(string request, float time)    // Function the play audio from a certain time (most is the same as PlaySound() )
+	{
+		if (Sound_Lib.ContainsKey(request))
+		{
+			GameObject clip = Instantiate(Sound_Prefab);
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];
+			clip.GetComponent<AudioSource>().time = time;                       // Only difference, get a time input and set the clip to be at the time
+			clip.GetComponent<AudioSource>().Play();
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+			ChangeVolume(1);
+		}
+	}
+
+
+	public void PlayFromTime(string request, float time, float loudness)    // Function the play audio from a certain time (most is the same as PlaySound() )
+	{
+		if (Sound_Lib.ContainsKey(request))
+		{
+			GameObject clip = Instantiate(Sound_Prefab);
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];
+			clip.GetComponent<AudioSource>().time = time;                       // Only difference, get a time input and set the clip to be at the time
+			clip.GetComponent<AudioSource>().volume = loudness;
+			clip.GetComponent<AudioSource>().Play();
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+			ChangeVolume(1);
+		}
+	}
+
+
+
+	public void PlayFromTime(string request, float time, float loudness, float pitchchange)    // Function the play audio from a certain time (most is the same as PlaySound() )
+	{
+		if (Sound_Lib.ContainsKey(request))
+		{
+			GameObject clip = Instantiate(Sound_Prefab);
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];
+			clip.GetComponent<AudioSource>().time = time;                       // Only difference, get a time input and set the clip to be at the time
+			clip.GetComponent<AudioSource>().volume = loudness;
+			clip.GetComponent<AudioSource>().pitch = pitchchange;
+			clip.GetComponent<AudioSource>().Play();
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+			ChangeVolume(1);
+		}
+	}
+
+
+
+	public void PlayWithDelay(string request, float delay)  // Function that plays sound with a delay (most is the same as PlaySound() )
+	{
+		if (Sound_Lib.ContainsKey(request))
+		{
+			GameObject clip = Instantiate(Sound_Prefab);
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];
+			clip.GetComponent<AudioSource>().PlayDelayed(delay);                // Only difference, played with a delay rather that right away
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+			ChangeVolume(1);
+		}
+	}
+
+	public void PlayWithDelay(string request, float delay, float loudness)  // Function that plays sound with a delay (most is the same as PlaySound() )
+	{
+		if (Sound_Lib.ContainsKey(request))
+		{
+			GameObject clip = Instantiate(Sound_Prefab);
+			clip.GetComponent<AudioSource>().clip = Sound_Lib[request];
+			clip.GetComponent<AudioSource>().volume = loudness;
+			clip.GetComponent<AudioSource>().PlayDelayed(delay);                // Only difference, played with a delay rather that right away
+			Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+			ChangeVolume(1);
+		}
+	}
+
+
+	public void ChangeVolume(float input)       // Function the change the clips volume
+	{
+		GameObject clip = Sound_Prefab;                         // gets the sound prefab	
+		clip.GetComponent<AudioSource>().volume = input;        // adjusts the volume to the input
+	}
+}
---
+++ JanJam/Assets/Scripts/CollectableScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour
+{
+
+	private ParticleSystem Particles;
+	private MeshRenderer Mesh;
+	private AudioManager SoundScript;
+	private MeshCollider MeshCollider;
+	private ScoringScript ScoreScript;
+
+
+	private void Start()
+	{
+		SoundScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioManager>();
+		Particles = GetComponent<ParticleSystem>();
+		Mesh = GetComponent<MeshRenderer>();
+		MeshCollider = GetComponent<MeshCollider>();
+		ScoreScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<ScoringScript>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Bike1")
+		{
+			if (other.gameObject.GetComponentInParent<UnicycleController>().P1Tier >= GetComponent<TierScript>().CollectTier)
+			{
+				SoundScript.PlaySound("Collect");
+				Mesh.enabled = false;
+				MeshCollider.enabled = false;
+
+				switch (GetComponent<TierScript>().CollectTier)
+				{
+					case Tiers.Tier1:
+						ScoreScript.Player1Scored(1);
+						break;
+					case Tiers.Tier2:
+						ScoreScript.Player1Scored(2);
+						break;
+					case Tiers.Tier3:
+						ScoreScript.Player1Scored(3);
+						break;
+					case Tiers.Tier4:
+						ScoreScript.Player1Scored(4);
+						break;
+					case Tiers.Tier5:
+						ScoreScript.Player1Scored(5);
+						break;
+					default:
+						break;
+				}
+
+				Particles.Play();
+				Destroy(gameObject, Particles.main.duration);
+			}
+		}
+		else if (other.gameObject.tag == "Bike2")
+		{
+			if (other.gameObject.GetComponentInParent<UnicycleController>().P2Tier >= GetComponent<TierScript>().CollectTier)
+			{
+				SoundScript.PlaySound("Collect");
+				Mesh.enabled = false;
+				MeshCollider.enabled = false;
+
+				switch (GetComponent<TierScript>().CollectTier)
+				{
+					case Tiers.Tier1:
+						ScoreScript.Player2Scored(1);
+						break;
+					case Tiers.Tier2:
+						ScoreScript.Player2Scored(2);
+						break;
+					case Tiers.Tier3:
+						ScoreScript.Player2Scored(3);
+						break;
+					case Tiers.Tier4:
+						ScoreScript.Player2Scored(4);
+						break;
+					case Tiers.Tier5:
+						ScoreScript.Player2Scored(5);
+						break;
+					default:
+						break;
+				}
+
+				Particles.Play();
+				Destroy(gameObject, Particles.main.duration);
+			}
+		}
+	}
+}
